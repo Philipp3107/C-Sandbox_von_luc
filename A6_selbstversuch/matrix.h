232153bc016631f709372533352eaf597e69feef
Philipp3107 <minephil31@gmail.com>
2022-12-07 08:46:32 +0100

    header anlegen

---
+++ A6_selbstversuch/matrix.h
@@ -0,0 +1,18 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+
+class Matrix{
+public:
+    Matrix();
+    Matrix(int x, int y);
+    ~Matrix();
+    void ausgabe();
+protected:
+    int m_zeilen, m_spalten;
+    float m_element[2];
+       
+
+};
+
+#endif /*_MATRIX_H_*/
