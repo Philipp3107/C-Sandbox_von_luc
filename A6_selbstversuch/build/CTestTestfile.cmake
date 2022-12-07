de335aba1b04c9f9bcc7f8df3f3fdcb6563e4841
Philipp3107 <minephil31@gmail.com>
2022-12-07 09:01:28 +0100

    commiting working projet with cmake

---
+++ A6_selbstversuch/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/philippkotte/Documents/Uni/C-Sandbox_from_luc/C-Sandbox_von_luc/A6_selbstversuch
+# Build directory: /Users/philippkotte/Documents/Uni/C-Sandbox_from_luc/C-Sandbox_von_luc/A6_selbstversuch/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
